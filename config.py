f187f9a97bebfd86ea7926fd83c88f876e789d81
merdobeyherdaim <acamert23@gmail.com>
2025-05-08 12:11:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+ # Bot ayarları
+import os
+
+# Telegram Bot API anahtarınızı buraya ekleyin
+API_KEY = "YOUR_BOT_API_KEY"
+
+# Lisans ve sunucu bilgileri
+OWNER = "@merdobey"
+SERVER_OWNER = "@SanalinMagandasi
